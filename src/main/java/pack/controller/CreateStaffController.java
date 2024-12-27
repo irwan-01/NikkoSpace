59dcf40d3c6f80f06ce0f1a651ab90c75d465bd4
irwan-01 <2023365691@student.uitm.edu.my>
2024-12-27 21:37:35 +0800

    Update and rename StaffController.java to CreateStaffController.java

--- src/main/java/pack/controller/CreateStaffController.java
+++ src/main/java/pack/controller/CreateStaffController.java
@@ -15,8 +15,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-@WebServlet("/StaffController")
-public class StaffController extends HttpServlet {
+@WebServlet("/CreateStaffController")
+public class CreateStaffController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -75,41 +75,5 @@ public class StaffController extends HttpServlet {
             RequestDispatcher dispatcher = request.getRequestDispatcher("staff_signup.jsp");
             dispatcher.forward(request, response);
         }
-    }
-
-    private void viewStaffProfile(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        if (session != null && session.getAttribute("staffId") != null) {
-            int staffId = (int) session.getAttribute("staffId");
-
-            try (Connection con = AzureSqlDatabaseConnection.getConnection();
-                 PreparedStatement ps = con.prepareStatement("SELECT * FROM staff WHERE staffId = ?")) {
-                ps.setInt(1, staffId);
-                ResultSet rs = ps.executeQuery();
-
-                if (rs.next()) {
-                    request.setAttribute("username", rs.getString("username"));
-                    request.setAttribute("email", rs.getString("email"));
-                    request.setAttribute("phoneNumber", rs.getString("phoneNumber"));
-                    request.setAttribute("birthDate", rs.getDate("birthDate"));
-                    request.setAttribute("gender", rs.getString("gender"));
-                    request.setAttribute("adminId", rs.getInt("adminId"));
-
-                    RequestDispatcher dispatcher = request.getRequestDispatcher("staff_profile.jsp");
-                    dispatcher.forward(request, response);
-                } else {
-                    response.sendRedirect("login.jsp");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                response.sendRedirect("error.jsp");
-            }
-        } else {
-            response.sendRedirect("login.jsp");
-        }
-    }
-
-    private void updateStaffProfile(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Code to update the staff profile (similar to the createStaff logic, but using an UPDATE query)
     }
 }
