32fa4c9a565ce1184b6492548206f556d54ef41a
irwan-01 <2023365691@student.uitm.edu.my>
2024-12-27 20:36:59 +0800

    Update SignupController.java

--- src/main/java/pack/controller/SignupController.java
+++ src/main/java/pack/controller/SignupController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import pack.connection.AzureSqlDatabaseConnection;
 import org.mindrot.jbcrypt.BCrypt;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.sql.Connection;
